8621cb1e68a561c1516b12762015609cc3cb7f68
Chris Weiss <Chrisweiss1984@gmail.com>
2017-08-02 23:20:05 -0600

    Added links to page to edit and delete, JSON links on every page,

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -5,15 +5,31 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 class Item(Base):
     __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
     category = Column(String(25))
     description = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+    	return{
+    		'id': self.id,
+    		'name':self.name,
+    		'description': self.description
+    	}
     
-
-
 engine = create_engine('sqlite:///itemcatalogwithusers.db')
 
 
--- vagrant/catalog/populate_items.py
+++ vagrant/catalog/populate_items.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import  Base, Item
+from database_setup import  Base, Item, User
 
 engine = create_engine('sqlite:///itemcatalogwithusers.db') 
 # Bind the engine to the metadata of the Base class so that the
@@ -18,28 +18,33 @@ DBSession = sessionmaker(bind=engine)
 # session.rollback()
 session = DBSession()
 
+#Create User
+User1 = User(name = "Chris", email = "email@example.com", picture = "example.jpg")
+session.add(User1)
+session.commit()
+
 
 # Create dummy user
-Item1 = Item(name="Goggles", category="Skiing", description = "They protect your eyes from sun and snow")
+Item1 = Item(name="Goggles", category="Skiing", description = "They protect your eyes from sun and snow", user_id = 1)
 session.add(Item1)
 session.commit()
 
-Item1 = Item(name="Skis", category="Skiing", description = "The go on your boots and help you ski")
+Item1 = Item(name="Skis", category="Skiing", description = "The go on your boots and help you ski", user_id = 1)
 session.add(Item1)
 session.commit()
 
-Item1 = Item(name="Basketball", category="Basketball", description = "Big orange ball")
+Item1 = Item(name="Basketball", category="Basketball", description = "Big orange ball", user_id = 1)
 session.add(Item1)
 session.commit()
 
-Item1 = Item(name="Net", category="Basketball", description = "You shoot the basketball through it")
+Item1 = Item(name="Net", category="Basketball", description = "You shoot the basketball through it", user_id = 1)
 session.add(Item1)
 session.commit()
 
-Item1 = Item(name="Swim Cap", category="Swimming", description = "Slows down resistance and makes you glide")
+Item1 = Item(name="Swim Cap", category="Swimming", description = "Slows down resistance and makes you glide", user_id = 1)
 session.add(Item1)
 session.commit()
 
-Item1 = Item(name="Goggles", category="Swimming", description = "Keeps the water out of your eyes")
+Item1 = Item(name="Goggles", category="Swimming", description = "Keeps the water out of your eyes", user_id = 1)
 session.add(Item1)
 session.commit()
